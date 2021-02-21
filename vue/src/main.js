70a9df60045d0b9d095dc1e8a263d83c03fa763a
seunghyun1003 <s961003-@naver.com>
2021-02-22 01:43:01 +0900

    환경설정

---
+++ vue/src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+import VeeValidate from 'vee-validate'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {} from '@fortawesome/free-solid-svg-icons'
+library.add()
+
+Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(VeeValidate)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
